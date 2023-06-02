8c44ebdc963a922bd5e40328649fa1aeb54ddc74
m4ym4y <m4ym4y@users.noreply.github.com>
2023-06-02 16:03:15 -0400

    colors and font

--- config.h
+++ config.h
@@ -1,21 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 6;        /* border pixel of windows */
+static const unsigned int gappx     = 12;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Monego:size=12" };
+static const char dmenufont[]       = "Monego:size=12";
+static const char col_gray1[]       = "#42395d";
+static const char col_gray2[]       = "#75507b";
+static const char col_gray3[]       = "#c2ffdf";
+static const char col_gray4[]       = "#f8f8f0";
+static const char col_cyan[]        = "#c2ffdf";
+static const char col_red[]         = "#ffb8d1";
+static const char col_purple[]      = "#8077a8";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_purple },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_red  },
 };
 
 /* tagging */
